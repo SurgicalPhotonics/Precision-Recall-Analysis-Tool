ad38f5c413f050688daabbc584beed4edbc9d8de
chrislearns <77995541+chrislearns@users.noreply.github.com>
2023-06-21 09:47:52 -0400

    Update _writer.py

--- src/tracking_challenge_tutorial_v1/_writer.py
+++ src/tracking_challenge_tutorial_v1/_writer.py
@@ -2,32 +2,41 @@
 This module is an example of a barebones writer plugin for napari.
 
 It implements the Writer specification.
-see: https://napari.org/stable/plugins/guides.html?#writers
+see: https://napari.org/plugins/guides.html?#writers
 
 Replace code below according to your needs.
 """
-from __future__ import annotations
+import os
+from shutil import make_archive, rmtree
+from typing import Any
 
-from typing import TYPE_CHECKING, Any, List, Sequence, Tuple, Union
-
-if TYPE_CHECKING:
-    DataType = Union[Any, Sequence[Any]]
-    FullLayerData = Tuple[DataType, dict, str]
-
-
-def write_single_image(path: str, data: Any, meta: dict) -> List[str]:
-    """Writes a single image layer"""
-
-    # implement your writer logic here ...
-
-    # return path to any file(s) that were successfully written
-    return [path]
+from napari.qt import thread_worker
+from tifffile import imsave
 
 
-def write_multiple(path: str, data: List[FullLayerData]) -> List[str]:
-    """Writes multiple layers of different types."""
+def write_single_image(path: str, data: Any, meta: dict):
+    if str(path).endswith(".zip"):
+        path = str(path)[:-4]
+    if data.ndim != 3:
+        return None
 
-    # implement your writer logic here ...
+    @thread_worker()
+    def write_tiffs(data, dir_pth):
+        for i, t_slice in enumerate(data):
+            tiff_nme = f"seg{str(i).zfill(3)}.tif"
+            tiff_pth = os.path.join(dir_pth, tiff_nme)
+            imsave(tiff_pth, t_slice)
 
-    # return path to any file(s) that were successfully written
-    return [path]
+    def zip_dir():
+        make_archive(path, "zip", path)
+        rmtree(path)
+
+    os.mkdir(path)
+    layer_dir_pth = os.path.join(path, "01_AUTO/SEG")
+    os.makedirs(layer_dir_pth)
+    if len(data.shape) == 2:
+        data = [data]
+    worker = write_tiffs(data, layer_dir_pth)
+    worker.start()
+    worker.finished.connect(zip_dir)
+    return path + ".zip"
